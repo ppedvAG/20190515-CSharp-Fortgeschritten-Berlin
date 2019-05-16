89967d2b5a1ba861dc89e451bb579260f2f72d6e
Markus Yaseen <MarkusY@ppedv.de>
2019-05-16 12:05:28 +0200

    Threadingprobleme zwei Threads

---
+++ Threading/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Threading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t = new Thread(new ParameterizedThreadStart(MachEtwas));
+            t.Start("A");
+            Thread t2 = new Thread(new ParameterizedThreadStart(MachEtwas));
+            t2.Start("B");
+            Console.ReadKey();
+        }
+
+        private static void MachEtwas(object prefix)
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                Console.WriteLine($"{prefix} {i}");
+            }
+
+        }
+    }
+}
